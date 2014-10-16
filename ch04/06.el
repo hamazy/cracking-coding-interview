e2bc93945d7a993c250423e6551fb1ddf72001e9
Suguru Hamazaki <hamazy@suguruhamazaki.com>
2014-10-16 13:51:58 +0900

    Add more test cases.

--- ch04/06.el
+++ ch04/06.el
@@ -28,11 +28,21 @@
           (grand-parent (cci-node-parent parent)))
       (let ((grand-parent-left (cci-node-left-tree grand-parent)))
         (if (equal node parent-left)
-            ;; the node is parent's left sub-tree
+            ;; if the current node is the root of parent's left
+            ;; sub-tree, then the next node resides in the right
+            ;; sub-tree of the current node. But if the current node
+            ;; doesn't have a right sub-tree, the parent node is the
+            ;; next one.
             (if my-right (cci-btree-min my-right) parent)
-          ;; the node is parent's right sub-tree
+          ;; if the node is the root of parent's right sub-tree, then
+          ;; the next node resides in the right sub-tree of the
+          ;; current node.
           (if my-right
               (cci-btree-min my-right)
+            ;; But if the right sub-tree is missing, the next is its
+            ;; grand parent only when the parent node resides in the
+            ;; left sub-tree of the grand parent. Otherwise, there's
+            ;; no next node.
             (if (equal parent grand-parent-left)
                 grand-parent
               nil)))))))
@@ -55,3 +65,33 @@
        (eq (cci-node-value (cci-bst-next n2)) 3)
        (eq (cci-node-value (cci-bst-next n4)) 5)
        (eq (cci-node-value (cci-bst-next n5)) 6)))
+
+(let ((n0 (cci-node-create 0))
+      (n1 (cci-node-create 1))
+      (n2 (cci-node-create 2)))
+  (cci-node-set-left-tree n2 n0)
+  (cci-node-set-right-tree n0 n1)
+  (and (cci-bstp n2)
+       (eq (cci-node-value (cci-bst-next n0)) 1)
+       (eq (cci-node-value (cci-bst-next n1)) 2)))
+
+(let ((n0 (cci-node-create 0))
+      (n1 (cci-node-create 1)))
+  (cci-node-set-left-tree n1 n0)
+  (and (cci-bstp n1)
+       (eq (cci-node-value (cci-bst-next n0)) 1)))
+
+(let ((n0 (cci-node-create 0))
+      (n1 (cci-node-create 1))
+      (n2 (cci-node-create 2)))
+  (cci-node-set-right-tree n0 n1)
+  (cci-node-set-right-tree n1 n2)
+  (and (cci-bstp n0)
+       (eq (cci-node-value (cci-bst-next n1)) 2)
+       (not (cci-bst-next n2))))
+
+(let ((n0 (cci-node-create 0))
+      (n1 (cci-node-create 1)))
+  (cci-node-set-right-tree n0 n1)
+  (and (cci-bstp n0)
+       (not (cci-bst-next n1))))
